0b95d57deb575b0940e31e65be644c080af6324f
Balaji <85604441+balajisuresh1359@users.noreply.github.com>
2021-07-07 11:41:22 +0530

    Finding nth fibonacci number using dynamic programming

--- fib.py
+++ fib.py
@@ -1,24 +1,24 @@
 import time
-def fib(n,memo={}):
+def fibdp(n,memo={}):
 	if n in memo:
 		return memo[n]
 	if n<=2:
 		return 1
-	memo[n] = fib(n-1,memo)+fib(n-2,memo)
+	memo[n] = fibdp(n-1,memo)+fibdp(n-2,memo)
 	return memo[n]
 
-def fibn(n):
+def fibnormal(n):
 	if n<=2:
 		return 1
-	return fibn(n-1)+fibn(n-2)
+	return fibnormal(n-1)+fibnormal(n-2)
 
 
 start = time.time()
-print(fib(20))
+print(fibnormal(20))
 print(str(time.time()-start))
 
 start = time.time()
-print(fibn(20))
+print(fibdp(20))
 print(str(time.time()-start))
 #1 1 2 3 5 8 13 21 34 55
 #1 2 3 4 5 6 7  8  9  10
